ccbe2366d9545873ed180315c1a267e6ca6b0a7c
AysuGafarli <133383250+aysugafarli23@users.noreply.github.com>
2024-05-15 00:37:01 +0400

    Update samitler.py

--- Packets/samitler.py
+++ Packets/samitler.py
@@ -1,5 +1,5 @@
 def samitleri_al(string):
-    vowels = ['a','e','i','o','u']
+    vowels = ['a','ə','e','ı','i','o','ö','u','ü']
     result = {letter for letter in string if letter.lower() not in vowels }
     # result = ''.join(result)
     return result
